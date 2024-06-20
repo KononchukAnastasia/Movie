8a94057c794f88c97e45c240a8ecaae45f1bfb8c
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-06-20 14:54:45 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Анастасия Конончук on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
