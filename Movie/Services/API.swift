acd31ce48f580cf27357640796b9a91b01c8f8b2
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-06-20 21:39:10 +0700

    Add NetworkManager.

--- Movie/Services/API.swift
+++ Movie/Services/API.swift
@@ -10,5 +10,20 @@ import Foundation
 struct API {
     // MARK: - Private Properties
     
-    private static var baseUrl = "https://s3-eu-west-1.amazonaws.com/sequeniatesttask/films.json"
+    private static var baseUrl = "https://s3-eu-west-1.amazonaws.com"
+    
+    // MARK: - Public Methods
+    
+    static func films() -> String {
+        createEndpoint(["sequeniatesttask", "films.json"])
+    }
+    
+    // MARK: - Private Methods
+    
+    private static func createEndpoint(_ components: [String]) -> String {
+        var components = components
+        components.insert(baseUrl, at: 0)
+        
+        return components.joined(separator: "/")
+    }
 }
--- Movie/Services/NetworkManager.swift
+++ Movie/Services/NetworkManager.swift
@@ -6,3 +6,97 @@
 //
 
 import Foundation
+
+enum NetworkError: LocalizedError {
+    // Invalid request, e.g. invalid URL.
+    case badURL(String = "Bad URL or nil.")
+    
+    // Indicates an error on the transport layer,
+    // e.g. not being able to connect to the server.
+    case transportError(Error)
+    
+    // Received an bad response, e.g. non HTTP result.
+    case badResponse(String, Int)
+    
+    // No decoded data.
+    case noDecodedData(String = "The data couldn’t be read because it isn’t in the correct format.")
+    
+    var message: String {
+        switch self {
+        case .badURL(let message):
+            return message
+        case .transportError(let error):
+            return error.localizedDescription
+        case .badResponse(let message, _):
+            return message
+        case .noDecodedData(let message):
+            return message
+        }
+    }
+    
+    var statusCode: Int? {
+        switch self {
+        case .badURL(_):
+            return nil
+        case .transportError(_):
+            return nil
+        case .badResponse(_, let statusCode):
+            return statusCode
+        case .noDecodedData(_):
+            return nil
+        }
+    }
+}
+
+final class NetworkManager {
+    // MARK: - Public Properties
+    
+    static let shared = NetworkManager()
+    
+    // MARK: - Initializers
+    
+    private init() {}
+    
+    // MARK: - Public Methods
+    
+    func fetchBox(
+        url: String,
+        completion: @escaping (Result<Box, NetworkError>) -> ()
+    ) {
+        guard let url = URL(string: url) else {
+            return completion(.failure(.badURL()))
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.transportError(error)))
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                let statusCode = httpResponse.statusCode
+                
+                if !(200...299).contains(statusCode) {
+                    let message = "Bad response.\nStatus code:"
+                    let error = "\(message) \(statusCode)"
+                    
+                    completion(.failure(.badResponse(error, statusCode)))
+                    return
+                }
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let box = try decoder.decode(Box.self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(.success(box))
+                }
+            } catch {
+                completion(.failure(.noDecodedData(error.localizedDescription)))
+            }
+        }.resume()
+    }
+}
